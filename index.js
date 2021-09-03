77bbd3ad66374b9463cf193b45096ff73915a1d6
Layna Oyervides <laynaoyervides@gmail.com>
2021-09-02 20:54:49 -0600

    commit tree 81fd1971a45d59016911ebd18bf9095fd380ecec

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Your code here
+// Arrays
+const csvDataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300]
+  ]
+
+  const csvTimesIn = [
+    ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
+    ["Natalia", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1300"]],
+    ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
+    ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
+  ]
+
+  const csvTimesOut = [
+    ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Natalia", ["2018-01-01 2300", "2018-01-02 2300", "2018-01-03 2300"]],
+    ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
+    ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
+  ]
+
+//Payroll Application
+function createEmployeeRecord (csvDataEmployees) {
+ 
+    const newRecord=
+        { 
+            firstName: csvDataEmployees[0],
+            familyName: csvDataEmployees[1],
+            title:  csvDataEmployees[2],
+            payPerHour: csvDataEmployees[3],
+            timeInEvents:[],
+            timeOutEvents:[],
+        }
+    return newRecord
+};
+
+function createEmployeeRecords (csvDataEmployees) {
+    const firstRecord = map.csvDataEmployees (createEmployeeRecord(csvDataEmployees));
+      return firstRecord;
+}
+
+function createTimeInEvent () {};
+function createTimeOutEvent () {};
+    function hoursWorkedOnDate (){};
+    function wagesEarnedOnDate (){};
+    function allWagesFor(){};
+function calculatePayroll(){};
+
+
+
+
